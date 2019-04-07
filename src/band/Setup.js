d5c23ae9398ae8e20e2329b4b24ccf3483ce9aa0
Rachel Kawula <rkawula@gmail.com>
2019-04-07 14:57:14 -0700

    Navbar: Added navbar, react-router-dom dependencies, and example hello world components for Discover and Setup.

---
+++ src/band/Setup.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class Setup extends Component {
+
+    render() {
+        return (
+            <div id="band-setup">
+                Hello world band setup!
+            </div>);
+    }
+}
+
+
+export default Setup;
